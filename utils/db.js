f0cc192736144b414feb3a1c0d5af1427e2150a0
feportoa <feporto.a@gmail.com>
2024-07-15 16:36:16 -0300

    Added utils folder and moved db.js

---
+++ utils/db.js
@@ -0,0 +1,42 @@
+const pgData = require('../psql-data.json')
+const { Client } = require('pg');
+
+const client = new Client({
+    user: pgData.user,
+    host: pgData.host,
+    port: pgData.port,
+    password: pgData.password,
+    database: pgData.database
+});
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Connected to PostgreSQL");
+    } catch (err) {
+        console.log("Error connecting to PostgreSQL: ", err);
+        throw err;
+    }
+}
+
+async function pgQuery(sql, params=null) {
+    try {
+        const res = await client.query(sql, params);
+        return res.rows;
+        // if (params == null) {
+        //     const res = await client.query(sql);
+        //     return res.rows;
+        // } else {
+        //     const res = await client.query(sql, params);
+        //     return res.rows;
+        // }
+    } catch (err) {
+        console.error("Error executing query: ", err);
+        throw err;
+    }
+}
+
+module.exports = {
+    connectDB,
+    pgQuery
+};
