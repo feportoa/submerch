faafd770a75bf300c0e003b41e907a8177045145
DaveSudney <davidsudney0@gmail.com>
2024-09-30 16:52:30 -0300

    Dave

--- public/js/main.js
+++ public/js/main.js
@@ -78,3 +78,15 @@ loadHeader();
     event.currentTarget.className += " active";
 }
 
+// JavaScript para alternar entre os formulários de login e registro
+document.getElementById('show-register').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.login-form').style.display = 'none';
+    document.querySelector('.register-form').style.display = 'block';
+});
+
+document.getElementById('show-login').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('.register-form').style.display = 'none';
+    document.querySelector('.login-form').style.display = 'block';
+});
